446257d926641f737d612c2c8786b43f90d8bec2
AlyssaD <pink_lime@live.com>
2022-10-10 16:42:37 -0400

    add living room

--- src/Floorplan.jsx
+++ src/Floorplan.jsx
@@ -1,17 +1,21 @@
 import Bedroom from "./Bedroom"
+import LivingRoom from "./LivingRoom"
 
 const Floorplan = (props) => {
   return (
     <>
-    <div>
-      <Bedroom roomNo={'1'}/>
-    </div>
-    <div>
-      <Bedroom roomNo={'2'}/>
-    </div>
-    <div>
-      <Bedroom roomNo={'3'}/>
-    </div>
+      <div>
+        <Bedroom roomNo={'1'}/>
+      </div>
+      <div>
+        <Bedroom roomNo={'2'}/>
+      </div>
+      <div>
+        <LivingRoom />
+      </div>
+      <div>
+        <Bedroom roomNo={'3'}/>
+      </div>
     </>
   )
 }
