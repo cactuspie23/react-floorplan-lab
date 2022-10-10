1938ef6ab3b84eba1fefed0bd462a51a7f273ed2
AlyssaD <pink_lime@live.com>
2022-10-10 16:57:23 -0400

    add kitchen

--- src/Floorplan.jsx
+++ src/Floorplan.jsx
@@ -1,12 +1,16 @@
 import Bedroom from "./Bedroom"
 import LivingRoom from "./LivingRoom"
 import Bath from "./Bath"
+import Kitchen from "./Kitchen"
 
 const Floorplan = (props) => {
   return (
     <>
       <div>
         <Bedroom roomNo={'1'}/>
+      </div>
+      <div>
+        <Kitchen />
       </div>
       <div>
         <Bath size={'Full'}/>
---
+++ src/Kitchen.jsx
@@ -0,0 +1,7 @@
+const Kitchen = (props) => {
+  return (
+    <h2>Kitchen</h2>
+  );
+}
+
+export default Kitchen;
