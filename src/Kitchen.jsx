ebd484334d862f2ee893fc1a0b0627152eacea02
AlyssaD <pink_lime@live.com>
2022-10-10 17:08:29 -0400

    add kitchen elements

--- src/Kitchen.jsx
+++ src/Kitchen.jsx
@@ -1,6 +1,17 @@
+import Sink from "./Sink"
+import Oven from "./Oven"
+
 const Kitchen = (props) => {
   return (
-    <h2>Kitchen</h2>
+    <>
+      <h2>Kitchen</h2>
+      <div>
+        <Oven />
+      </div>
+      <div>
+        <Sink />
+      </div>
+    </>
   );
 }
 
